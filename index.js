0cb65cd3b2a693866d5312eddc73696dbcc0d249
Carolina Reyes <carolina.reyes@carolina.reyes>
2021-03-04 17:40:16 -0600

    add smile face

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => res.send('Holi'));
+app.get('/', (req, res) => res.send('Holi :)'));
 app.listen(port, () => console.log(port));
